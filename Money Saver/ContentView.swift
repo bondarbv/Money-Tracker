2e3d57fe48573fbd7324f389cbe2d1af61626645
Bohdan Bondar <bondarbv@icloud.com>
2023-07-17 22:36:00 +0300

    project init

---
+++ Money Saver/ContentView.swift
@@ -0,0 +1,30 @@
+//
+// Money Saver
+// ContentView.swift
+
+// Created by Bohdan Bondar on 17.07.2023 at 10:31 PM.
+// Copyright (c) 2023 Bohdan Bondar. All rights reserved.
+ 
+// GitHub: https://github.com/bondarbv
+// Linkedin: https://www.linkedin.com/in/bondarbv-ios/
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
