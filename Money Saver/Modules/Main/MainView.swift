bf4e22a12f66cb64a08379fda82beaaed3b25100
Bohdan Bondar <bondarbv@icloud.com>
2023-07-17 22:40:46 +0300

    folders

--- Money Saver/Modules/Main/MainView.swift
+++ Money Saver/Modules/Main/MainView.swift
@@ -1,17 +1,6 @@
-//
-// Money Saver
-// ContentView.swift
-
-// Created by Bohdan Bondar on 17.07.2023 at 10:31 PM.
-// Copyright (c) 2023 Bohdan Bondar. All rights reserved.
- 
-// GitHub: https://github.com/bondarbv
-// Linkedin: https://www.linkedin.com/in/bondarbv-ios/
-
-
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -25,6 +14,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
